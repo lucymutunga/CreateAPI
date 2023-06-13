34883c7b83eee7c55dade4a59a31fd33a8d698b6
lucymutunga <lucyalphonce18@gmail.com>
2023-06-13 10:38:36 +0300

    adding files

---
+++ data.js
@@ -0,0 +1,25 @@
+const todoTasks = [
+    {
+      id: 1,
+      title: "Task 1",
+      description: "Lorem ipsum dolor sit amet",
+      priority: "High",
+      completed: false
+    },
+    {
+      id: 2,
+      title: "Task 2",
+      description: "Consectetur adipiscing elit",
+      priority: "Medium",
+      completed: true
+    },
+    {
+      id: 3,
+      title: "Task 3",
+      description: "Sed do eiusmod tempor incididunt",
+      priority: "Low",
+      completed: false
+    },
+   
+  ];
+  module.exports = todoTasks;
